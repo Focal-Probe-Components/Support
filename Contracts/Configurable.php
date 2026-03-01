2fcb7d8ad1ed235eb630ec754d11117a402efedd
Patryk <contact@patryknamyslak.pl>
2026-03-01 18:59:58 +0000

    Added Config Binding

--- Contracts/Configurable.php
+++ Contracts/Configurable.php
@@ -20,8 +20,20 @@ abstract class Configurable{
      * Bind the config or configs using either $configPath or $configPaths
      * @param string $path
      * @return void
+     * @throws InvalidArgumentException When `$name` or `$path` is empty
      */
-    abstract public function bindConfig(string $path);
+    public function bindConfig(string $name = "", string $path): void{
+        if (Str::isEmpty($path)){
+            throw new InvalidArgumentException('$path cannot be left empty in bindConfig() on Configurable class: ' . static::class);
+        }
+        if (!static::$multiConfigMode){
+            static::$configPath = $path;
+        }
+        if (Str::isEmpty($name)){
+            throw new InvalidArgumentException('$name cannot be left empty in bindConfig() whilst using multi config mode on Configurable class: ' . static::class);
+        }
+        static::$configPaths[$name] = $path;
+    }
 
     /**
      * Get a binded config path by name i.e `app => /path/to/config.php`, `$name` would be `app` and the return of `Configurable::config('app')` would be `/path/to/config.php`
@@ -30,8 +42,8 @@ abstract class Configurable{
      */
     public function config(string $name = ""): string{
         return match(true){
-            self::$multiConfigMode and in_array($name, self::$configPaths) => self::$configPaths[$name],
-            !self::$multiConfigMode => self::$configPath,
+            static::$multiConfigMode and in_array($name, static::$configPaths) => static::$configPaths[$name],
+            !static::$multiConfigMode => static::$configPath,
             // If its in multi config mode and the $name provided is not in the binded configPaths
             default => throw new InvalidArgumentException("$name is not a valid Binded config for Configurable class: " . static::class),
         };
